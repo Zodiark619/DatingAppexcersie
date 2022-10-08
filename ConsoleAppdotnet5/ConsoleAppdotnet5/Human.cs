f9158e0d144176d2cd68c53dd8051381a65c7c6a
Zodiark619 <seonghyejin33@gmail.com>
2022-10-09 06:56:00 +0700

    erer

---
+++ ConsoleAppdotnet5/ConsoleAppdotnet5/Human.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppdotnet5
+{
+
+    class Employee
+    {
+        public string EmployeeName { get; set; }
+        public int EmployeeAge { get; set; }
+        public string EmployeeOccupation { get; set; }
+        public Employee()
+        {
+            this.EmployeeName = "Jaya";
+            this.EmployeeAge = 24;
+            this.EmployeeOccupation="Killleee";
+        }
+        public void Friendship(bool isFriend)
+        {
+            if (isFriend) Console.WriteLine(meong());
+            else Console.WriteLine("Kmzway87aaa"); 
+        }
+        private string meong()
+        {
+            return $"Hi,I'm {EmployeeName}, age {EmployeeAge}, occupation {EmployeeOccupation}";
+        }
+    }
+
+
+    class Box
+    {
+      
+        public int Length { get; set; }
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+        public int Volume
+        {
+            get
+            {
+           return this.Length*this.Height*this.Width;
+                
+            }
+        }
+        public int FrontSize
+        {
+            get
+            {
+                return this.Length * this.Height;
+            }
+        }
+        public void result()
+        {
+            Console.WriteLine("Length is {0} width {1} height {2} and volume {3} frontsize {4}",this.Length,this.Width,this.Height,this.Volume,this.FrontSize);
+        }
+    }
+
+    internal class Human
+    {
+        private string firstname;
+        private string lastname;
+        private string eyecolor;
+        private int age;
+        public Human()
+        {
+            Console.WriteLine("-------------");
+        }
+        public Human(string firstname, string lastname)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            
+        }
+        public Human(string firstname, string lastname, string eyecolor)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.eyecolor = eyecolor;
+           
+        }
+        public Human(string firstname, string lastname, string eyecolor, int age)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.eyecolor = eyecolor;
+            this.age = age;
+        }
+
+        public void Introduce()
+        {
+            if(age != 0 && firstname !=null &&lastname!=null&&eyecolor!=null)
+               
+                Console.WriteLine("Hi, I'm {0} {1} and I'm {2} years old. My eyecolor is {3}.", firstname, lastname, age, eyecolor);
+            else if (firstname != null && lastname != null && eyecolor != null)
+
+                Console.WriteLine("Hi, I'm {0} {1} and I'm {2} years old. ", firstname, lastname, age);
+            else if ( firstname != null && lastname != null)
+
+                Console.WriteLine("Hi, I'm {0} {1} just just ", firstname, lastname);
+            else
+                Console.WriteLine("Empty hhuuummaann");
+
+
+        }
+
+    }
+}
---
+++ ConsoleAppdotnet5/ConsoleAppdotnet5/susu.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppdotnet5
+{
+    static class susu1
+    {
+        public static void Hihi3()
+        {
+        char[,] meong = { { '1', '2', '3' }, { '4', '5', '6' }, { '7', '8', '9' } };
+            susu.SetField();
+
+    }
+
+
+
+    public static bool Hihi2()
+        {
+            char[] koko = { 'X', 'O' };
+
+            foreach(char c in koko)
+            {
+                if(susu.meong[0, 0]==c&& susu.meong[0, 1]==c&& susu.meong[0, 2]==c ||
+                    susu.meong[1, 0] == c && susu.meong[1, 1] == c && susu.meong[1, 2] == c ||
+                    susu.meong[2, 0] == c && susu.meong[2, 1] == c && susu.meong[2, 2] == c ||
+                    susu.meong[0, 0] == c && susu.meong[1, 0] == c && susu.meong[2, 0] == c ||
+                    susu.meong[0, 1] == c && susu.meong[1, 1] == c && susu.meong[2, 1] == c ||
+                    susu.meong[0, 2] == c && susu.meong[1, 2] == c && susu.meong[2, 2] == c ||
+                    susu.meong[0, 0] == c && susu.meong[1,1] == c && susu.meong[2,2] == c ||
+                    susu.meong[0, 2] == c && susu.meong[1,1] == c && susu.meong[2, 0] == c )
+                {
+                    if (c == 'X')
+                    {
+                        susu.SetField();
+
+                        Console.WriteLine($"Player 2 has won!!");
+                        return true;
+                    }
+                    else if (c == 'O')
+                    {
+                        susu.SetField();
+
+                        Console.WriteLine($"Player 1 has won!!");
+                        return true;
+                    }
+                }
+            }
+            return false;
+            
+
+        }
+
+
+        public static int Hihi1(int player)
+        {
+         
+            while (true)
+            {
+                //int input = Convert.ToInt32(Console.ReadLine());
+                Console.Write($"Player {player}, pick which number: ");
+
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out int sisi))
+                {
+                    if (sisi < 1 || sisi > 10)
+                    {
+                        Console.WriteLine("numbers within 1-9 only");
+
+                        continue;
+                    }
+                    return sisi;
+                   
+
+                }
+                else
+                {
+                    Console.WriteLine("wrong input,only integer");
+                }
+            }
+            
+        }
+
+        public static bool Hihi(int player,int input)
+        {
+            char meong=' ';
+            if (player == 1) meong = 'X';
+            else if (player == 2) meong = 'O';
+
+            switch (input)
+            {
+                case 1:
+                    if (susu.meong[0, 0]!='X'&& susu.meong[0, 0]!='O')
+                    {
+                        susu.meong[0, 0] = meong;
+                        return true;
+
+                    }
+                   
+
+                        return false;
+                   
+                    
+                case 2:
+                    if (susu.meong[0, 1] != 'X' && susu.meong[0, 1] != 'O')
+                    {
+                        susu.meong[0, 1] = meong;
+                        return true;
+
+                    }
+                   
+
+                        return false;
+                    
+                case 3:
+                    if (susu.meong[0, 2] != 'X' && susu.meong[0, 2] != 'O')
+                    {
+                        susu.meong[0, 2] = meong;
+                        return true;
+
+                    }
+                   
+
+                        return false;
+                   
+                case 4:
+                    if (susu.meong[1, 0] != 'X' && susu.meong[1, 0] != 'O')
+                    {
+                        susu.meong[1, 0] = meong;
+                        return true;
+
+                    }
+                    
+
+                        return false;
+                   
+                case 5:
+                    if (susu.meong[1, 1] != 'X' && susu.meong[1,1] != 'O')
+                    {
+                        susu.meong[1,1] = meong;
+                        return true;
+
+                    }
+                   
+
+                        return false;
+                  
+                case 6:
+                    if (susu.meong[1, 2] != 'X' && susu.meong[1,2] != 'O')
+                    {
+                        susu.meong[1,2] = meong;
+                        return true;
+
+                    }
+                    
+
+                        return false;
+                  
+                case 7:
+                    if (susu.meong[2, 0] != 'X' && susu.meong[2, 0] != 'O')
+                    {
+                        susu.meong[2, 0] = meong;
+                        return true;
+
+                    }
+                   
+
+                        return false;
+                   
+                case 8:
+                    if (susu.meong[2,1] != 'X' && susu.meong[2,1] != 'O')
+                    {
+                        susu.meong[2,1] = meong;
+                        return true;
+
+                    }
+                    
+
+                        return false;
+                   
+                case 9:
+                    if (susu.meong[2,2] != 'X' && susu.meong[2,2] != 'O')
+                    {
+                        susu.meong[2,2] = meong;
+                        return true;
+
+                    }
+                   
+
+                        return false;
+                    
+                
+            }
+            return false;
+        }
+        
+
+    }
+
+    internal static class susu
+    {
+        public static char[,] meong = { { '1','2','3' }, { '4','5','6' } ,{ '7','8','9' } };
+        public static void SetField()
+        {
+            Console.WriteLine();
+            Console.WriteLine("|~~~~~|~~~~~|~~~~~|");
+            Console.WriteLine("|     |     |     |");
+
+            Console.WriteLine("|  {0}  |  {1}  |  {2}  |", meong[0, 0], meong[0, 1], meong[0,2]);
+            Console.WriteLine("|     |     |     |");
+            Console.WriteLine("|     |     |     |");
+
+            Console.WriteLine("|~~~~~|~~~~~|~~~~~|");
+            Console.WriteLine("|     |     |     |");
+
+            Console.WriteLine("|  {0}  |  {1}  |  {2}  |", meong[1, 0], meong[1, 1], meong[1, 2]);
+            Console.WriteLine("|     |     |     |");
+
+            Console.WriteLine("|     |     |     |");
+
+            Console.WriteLine("|~~~~~|~~~~~|~~~~~|");
+            Console.WriteLine("|     |     |     |");
+            Console.WriteLine("|  {0}  |  {1}  |  {2}  |", meong[2, 0], meong[2, 1], meong[2, 2]);
+            Console.WriteLine("|     |     |     |");
+            Console.WriteLine("|     |     |     |");
+
+
+            Console.WriteLine("|~~~~~|~~~~~|~~~~~|");
+
+        }
+    }
+}
